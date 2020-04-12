07b911ddd548e7bff8d1db13e98acf4fee82aca8
Lyès ubuntu <lyes.sardi@gmail.com>
2020-04-12 21:14:50 +0200

    tout sauf q3

--- main.py
+++ main.py
@@ -1,55 +1,65 @@
-from pyspark.shell import spark
 import pyspark.sql.functions as f
-from pyspark.sql.types import *
-from math import sin, cos, acos, pi
 from pyspark.sql import Window
 from pyspark.sql.types import DoubleType
+from pyspark.sql import Row
+from math import radians, cos, sin, asin, sqrt
+from pyspark.sql import SparkSession
 
-#############################################################################
-def deg2rad(dd):
-    """Convertit un angle "degrés décimaux" en "radians"
+spark = SparkSession.builder.appName('abc').getOrCreate()
+sc = spark.sparkContext
+
+def haversine(lon1, lat1, lon2, lat2):
     """
-    return dd / 180 * pi
-
-
-#############################################################################
-def distanceGPS(latA, longA, latB, longB):
-    """Retourne la distance en KM entre les 2 points A et B connus grâce à
-       leurs coordonnées GPS (en radians).
+    Calculate the great circle distance between two points
+    on the earth (specified in decimal degrees)
     """
-    # Rayon de la terre en mètres (sphère IAG-GRS80)
-    RT = 6378137
-
-    latA = deg2rad(latA)
-    longA = deg2rad(longA)
-
-    latB = deg2rad(latB)  # Nord
-    longB = deg2rad(longB)  # Est
-
-    # angle en radians entre les 2 points
-    S = acos(sin(latA) * sin(latB) + cos(latA) * cos(latB) * cos(abs(longB - longA)))
-    # distance entre les 2 points, comptée sur un arc de grand cercle
-    return S * RT / 1000
+    # convert decimal degrees to radians
+    lon1, lat1, lon2, lat2 = map(radians, [lon1, lat1, lon2, lat2])
+    # haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    # Radius of earth in kilometers is 6371
+    km = 6371 * c
+    return km
 
 
 train = (spark.read.option("inferSchema", "true")
          .option("header", "true")
          .csv("train.csv"))
 
-distanceGPS_UDF = f.udf(distanceGPS, DoubleType())
-
+distanceGPS_UDF = f.udf(haversine, DoubleType())
 train = train.withColumn('time_in_min', f.col('trip_duration') / 60) \
     .withColumn('KM', distanceGPS_UDF(
-    f.col('pickup_latitude'), f.col('pickup_longitude'), f.col('dropoff_latitude'), f.col('dropoff_longitude'))) \
+    f.col('pickup_longitude'), f.col('pickup_latitude'), f.col('dropoff_longitude'), f.col('dropoff_latitude'))) \
     .withColumn('KMH_AVG', f.col('KM') * 60 / f.col('time_in_min')) \
     .withColumn('DayOfW',
                 f.from_unixtime(f.unix_timestamp("pickup_datetime", "MM/dd/yyyy hh:mm:ss"), "EEEEE").alias("dow"))
 
-train.show()
+df = train.groupBy('DayOfW').count().withColumnRenamed("count", "q2")
+
+df2 = (train
+       .groupBy("DayOfW")
+       .agg(f.sum("KM")))
+df1 = train.join(df, df.DayOfW == train.DayOfW, how='left')
+
+row = Row("id", "start", "some_value")
+tst = sc.parallelize([
+    row(1, "2015-01-01 00:00:01", 20.0),
+    row(1, "2015-01-01 00:00:02", 10.0),
+    row(1, "2015-01-01 00:00:03", 25.0),
+    row(1, "2015-01-01 00:00:04", 30.0),
+    row(2, "2015-01-01 00:00:05", 5.0),
+    row(2, "2015-01-01 00:00:06", 30.0),
+    row(2, "2015-01-01 00:00:07", 20.0)
+]).toDF().withColumn("start", f.col("start").cast("timestamp"))
+
+w = (Window()
+     .partitionBy(f.col("id"))
+     .orderBy(f.col("start").cast("long"))
+     .rangeBetween(-2, 0))
 
 
-#df = train.groupBy('DayOfW').count().withColumnRenamed("count","q2")
-
-#train1 = df.join(train, f.col(df.DayOfW) == f.col(train.DayOfW))
-
-train.describe()
+tst.select(f.col("*"),f.col('start').cast('long'), f.count("id").over(w).alias("mean")).show()
+tst.printSchema()
